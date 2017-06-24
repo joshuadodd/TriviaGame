1084ec00669956df3a45351ef9e21ee7b3394843
joshua dodd <joshuadodd@joshuas-MacBook-Pro.local>
2017-06-24 08:46:34 -0400

    final-ish

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,88 +1,69 @@
-$(document).ready(function(){
-answers = new Object();
-$('.option').change(function(){
-    var answer = ($(this).attr('value'))
-    var question = ($(this).attr('name'))
-    answers[question] = answer
-})
-var item1 = document.getElementById('questions');
+$(document).ready(function() {
+    answers = new Object();
+    $('.option').change(function() {
+        var answer = ($(this).attr('value'))
+        var question = ($(this).attr('name'))
+        answers[question] = answer
+    })
+    var item1 = document.getElementById('questions');
 
-var totalQuestions = $('.questions').size();
-var currentQuestion = 0;
-$questions = $('.questions');
-$questions.hide();
-$($questions.get(currentQuestion)).fadeIn();
-$('#next').click(function(){
-    $($questions.get(currentQuestion)).fadeOut(function(){
-        currentQuestion = currentQuestion + 1;
-        if(currentQuestion == totalQuestions){
-               var result = sum_values()
-               //do stuff with the result
-               alert(result);
-        }else{
-        $($questions.get(currentQuestion)).fadeIn();
-        }
+    var totalQuestions = $('.questions').size();
+    var currentQuestion = 0;
+    $questions = $('.questions');
+    $questions.hide();
+    $($questions.get(currentQuestion)).fadeIn();
+    $('#next').click(function() {
+        $($questions.get(currentQuestion)).fadeOut(function() {
+            currentQuestion = currentQuestion + 1;
+            if (currentQuestion == totalQuestions) {
+                var result = sum_values()
+                    //do stuff with the result
+                alert(result);
+            } else {
+                $($questions.get(currentQuestion)).fadeIn();
+            }
+        });
+
     });
+});
+$("#btn btn-info").on("click", function() {
+    // when the start button clicked, the div with the questions that was hidden is shown
+    $('.questions').show();
+    console.log('hello');
+
+    $(this).hide();
+
 
 });
-});
-$(".start-button").on("click", function() {
-        // when the start button clicked, the div with the questions that was hidden is shown
-        $('main-container').show();
-        console.log('hello');
+function startTimer(duration, display) {
+        $("#start-button").on("click", function() {
 
-        $(this).hide();
-    });
+           function startTimer(secs) {
+                timeInSecs = parseInt(secs) - 1;
+                ticker = setInterval("tick()", 1000); // every second
+            }
 
-    // These events start the timer: set the number of seconds the guesser has 
-    var number = 30;
-    $('#timeLeft').on('click', run);
+           function tick() {
+                var secs = timeInSecs;
+                if (secs > 0) {
+                    timeInSecs--;
+                } else {
+                    clearInterval(ticker); // stop counting at zero
+                    // startTimer(30);  // remove forward slashes in front of startTimer to repeat if required
+                }
+                $("#countdownTimer").html(secs);
+            }
+            startTimer(30); //30 seconds
+            console.log(secs, timeInSecs, ticker);
+        })
 
-    // This function enables the number of seconds to decrease with time, and to display
-    // the result of that decrease until time is up. 
-    function decrement() {
-        // Decrease number by one.
-        number--;
-        // Show the number in the #timeLeft div.
-        $('#timeLeft').html('<h2>' + number + " seconds" + '</h2>');
-        // When the number is equal to zero, 
-        if (number === 0) {
-            // run the stop function.
-            stop();
-            // Alert the user that time is up. Update the innerHTML of the message
-            // div to say 'Game Over!'
-            // alert('Time Up!')
-            $('#message').html('time up!');
-            checkAnswers();
-        }
+   }
+
+
+function sum_values() {
+    var the_sum = 0;
+    for (questions in answers) {
+        the_sum = the_sum + parseInt(answers[question])
     }
-    // test
-    // writes the win or lose message 
-    // function writeMessage (){
-    //  // updates the contents of the message div
-    //  $message.html(message);
-    // }
-    // test
-
-    // the run function sets the spacing of the decrement function's time interval so that
-    // it can be equal to a second per number decrement.
-    function run() {
-        counter = setInterval(decrement, 1000);
-    }
-
-    // The stop function
-    function stop() {
-        // Clears our "counter" interval. The interval name is passed to the clearInterval function.
-        clearInterval(counter);
-    }
-
-    // Execute the run function.
-    run();
-
-function sum_values(){
-var the_sum = 0;
-for (questions in answers){
-    the_sum = the_sum + parseInt(answers[question])
-}
-return the_sum
+    return the_sum
 }
